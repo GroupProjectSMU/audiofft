1d2e4dcacaa47d3f353fae72688fdd8f8027b9ef
Dou <jackwangshanghai@gmail.com>
2023-09-18 17:07:54 -0500

    Initial Commit

---
+++ audiofft/audiofftApp.swift
@@ -0,0 +1,17 @@
+//
+//  audiofftApp.swift
+//  audiofft
+//
+//  Created by Yihao Wang on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct audiofftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
