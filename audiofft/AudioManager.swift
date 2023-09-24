fd9c8785f09c8470f2caf4d24270b84cdd44a155
Dou <jackwangshanghai@gmail.com>
2023-09-23 20:37:02 -0500

    Save changes

--- audiofft/AudioManager.swift
+++ audiofft/AudioManager.swift
@@ -8,21 +8,19 @@
 import SwiftUI
 import AVFoundation
 import Accelerate
+import Charts
 
 class dspSplitComplexHolder{
-    var realp: [Float]
-    var imagp: [Float]
+
     let realPtr: UnsafeMutablePointer<Float>
     let imagPtr: UnsafeMutablePointer<Float>
     var complexValue: DSPSplitComplex
     
     init(length: Int) {
-        realp = Array(repeating: 0.0, count: length)
-        imagp = Array(repeating: 0.0, count: length)
         realPtr = UnsafeMutablePointer<Float>.allocate(capacity: length)
         imagPtr = UnsafeMutablePointer<Float>.allocate(capacity: length)
-        realPtr.initialize(from: realp, count: length)
-        imagPtr.initialize(from: imagp, count: length)
+        realPtr.initialize(repeating: 0.0, count: length)
+        imagPtr.initialize(repeating: 0.0, count: length)
         complexValue = DSPSplitComplex(realp: realPtr, imagp: imagPtr)
     }
     
@@ -34,42 +32,74 @@ class dspSplitComplexHolder{
 
 class AudioEngineManager: ObservableObject {
     var audioEngine = AVAudioEngine()
-    var fftSetup: FFTSetup!
-    let fftLength = vDSP_Length(4096)
     
     var fftInstance = vDSP.FFT(log2n: 12, radix: .radix2, ofType: DSPSplitComplex.self)
     var output: [Float] = Array(repeating: 0.0, count: 2048)  // half fftLength
     
-    var inputComplex: dspSplitComplexHolder = dspSplitComplexHolder(length: 4096)
-    var outputComplex: dspSplitComplexHolder = dspSplitComplexHolder(length: 4096)
-    
+    var inputComplex: dspSplitComplexHolder!
+    var outputComplex: dspSplitComplexHolder!
+    var landMarkModel: Bool = false
     @Published var outputLog: [Float] = Array(repeating: 0.0, count: 2048)  // half fftLength
+    @Published var lineMarks: [LineMark] = []
+    @Published var initDone: Bool = false
     init() {
-        setupEngine()
-        startEngine()
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [])
+            try audioSession.setActive(true)
+            print("Permission OK")
+            setupEngine(length: 4096)
+            startEngine()
+            initDone = true
+        } catch {
+            print("Error setting up audio session: \(error)")
+        }
     }
     
-    func setupEngine() {
+    init(landMarkModel: Bool) {
+        self.landMarkModel = landMarkModel
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(.playAndRecord, mode: .default, options: [])
+            try audioSession.setActive(true)
+            print("Permission OK")
+            setupEngine(length: 4096)
+            startEngine()
+            initDone = true
+        } catch {
+            print("Error setting up audio session: \(error)")
+        }
+    }
+    
+    func setupEngine(length: Int) {
+        inputComplex =  dspSplitComplexHolder(length: length)
+        outputComplex = dspSplitComplexHolder(length: length)
         let inputNode = audioEngine.inputNode
         let inputFormat = inputNode.outputFormat(forBus: 0)
-//        fftSetup = vDSP_create_fftsetup(log2n, FFTRadix(kFFTRadix2))
         
-        inputNode.installTap(onBus: 0, bufferSize: UInt32(fftLength), format: inputFormat) { [unowned self] buffer, _ in
+        print("Sampling Frequency: \(inputFormat.sampleRate)")
+        
+        inputNode.installTap(onBus: 0, bufferSize: UInt32(length), format: inputFormat) { [unowned self] buffer, _ in
             guard let floatData = buffer.floatChannelData?[0] else {return }
-//            let length = vDSP_Length(buffer.frameLength)
-            
             vDSP_mmov(floatData, inputComplex.realPtr, vDSP_Length(buffer.frameLength), 1, 1, vDSP_Length(buffer.frameLength))
-            DispatchQueue.global(qos: .default).async { [unowned self] in
-                fftInstance?.transform(input: inputComplex.complexValue, output: &outputComplex.complexValue, direction: .forward)
-                vDSP.squareMagnitudes(outputComplex.complexValue, result: &self.output)
-                DispatchQueue.main.async { [unowned self] in
-                    vDSP.convert(amplitude: self.output, toDecibels: &self.outputLog, zeroReference: 1.0)
-                }
-            }
         }
 
     }
     
+    func updateFFT(){
+        DispatchQueue.global(qos: .default).async { [unowned self] in
+            fftInstance?.transform(input: inputComplex.complexValue, output: &outputComplex.complexValue, direction: .forward)
+            vDSP.squareMagnitudes(outputComplex.complexValue, result: &self.output)
+            DispatchQueue.main.async { [unowned self] in
+                vDSP.convert(amplitude: self.output, toDecibels: &self.outputLog, zeroReference: 1.0)
+                if landMarkModel{
+                    lineMarks = outputLog.prefix(outputLog.count / 2).enumerated().map { (index, value) in
+                        LineMark(x: .value("Index", index), y: .value("Value", value))
+                    }
+                }
+            }
+        }
+    }
     func startEngine() {
         do {
             try audioEngine.start()
--- audiofft/ContentView.swift
+++ audiofft/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     var body: some View {
@@ -22,19 +23,19 @@ struct ContentView: View {
 
 struct FFTView: View {
     @ObservedObject var audioManager = AudioEngineManager()
-    
+    let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
     var body: some View {
         VStack {
             GeometryReader { geometry in
                 Path { path in
                     let width = geometry.size.width / CGFloat(audioManager.output.count / 2 - 512) // Dividing by 2 to only use half
-                    
+
                     // Initialize the starting point for the path
                     if let firstValue = audioManager.outputLog.first {
                         let height = CGFloat(firstValue) * geometry.size.height / 250.0
                         path.move(to: CGPoint(x: 0, y: geometry.size.height - height - 150))
                     }
-                    
+
                     // Looping only over the first half
                     for (index, value) in audioManager.outputLog.prefix(audioManager.output.count / 2 - 512).enumerated() {
                         let height = CGFloat(value) * geometry.size.height / 250.0  // Adjust as per your needs
@@ -43,12 +44,37 @@ struct FFTView: View {
                 }
                 .stroke(Color.blue)
             }
+            .onReceive(timer){ _ in
+                audioManager.updateFFT()
+            }
             .clipped()
             .drawingGroup()
         }
 
     }
 }
+
+//struct FFTView: View {
+//    @ObservedObject var audioManager = AudioEngineManager(landMarkModel: true)
+//    var body: some View {
+//        let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
+//        VStack {
+//            Chart{
+//                ForEach(0..<audioManager.lineMarks.count, id: \.self) { index in
+//                    audioManager.lineMarks[index]
+//                }
+//            }
+//            .chartYScale(domain: -150 ... 150)
+//            .chartXScale(domain: 0...1200)
+//            .clipped()
+//            .onReceive(timer, perform: { _ in
+//                audioManager.updateFFT()
+//            })
+//            .drawingGroup()
+//        }
+//    }
+//}
+
 
 
 struct ContentView_Previews: PreviewProvider {
